ccd18d514e156ad3a9a1b59360dab20012503f78
Wiemer Steenbeek <wiemer.steenbeek@trivesit.nl>
2025-04-03 13:09:01 +0200

    ModelVersionChecker verwijderd

--- SprintrExtension/MyDockablePaneExtensionWebViewModel.cs
+++ SprintrExtension/MyDockablePaneExtensionWebViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mendix.StudioPro.ExtensionsAPI.UI.DockablePane;
+﻿using Mendix.StudioPro.ExtensionsAPI.UI.DockablePane;
 using Mendix.StudioPro.ExtensionsAPI.UI.WebView;
 
 namespace SprintrExtension
